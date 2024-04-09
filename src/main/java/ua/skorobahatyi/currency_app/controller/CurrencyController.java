55aeb6edd8029674538f7b052e8af0cbe526af94
SkorobagatiyMykola <nick1980457@gmail.com>
2024-04-09 20:54:46 +0300

    Added Controller

--- src/main/java/ua/skorobahatyi/currency_app/controller/CurrencyController.java
+++ src/main/java/ua/skorobahatyi/currency_app/controller/CurrencyController.java
@@ -1,20 +1,25 @@
 package ua.skorobahatyi.currency_app.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ua.skorobahatyi.currency_app.entity.Currency;
+import ua.skorobahatyi.currency_app.entity.dto.CurrencyDto;
+import ua.skorobahatyi.currency_app.service.CurrencyService;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/v1/currency")
 public class CurrencyController {
 
+    private final CurrencyService currencyService;
     @GetMapping("")
-    public List<Currency> getRateCurrencyByDate(){
-        return null;
+    public List<CurrencyDto> getRateCurrencyByDate(){
+        return currencyService.getAllCurrency();
 
     }
     @GetMapping("/actual")
--- src/main/java/ua/skorobahatyi/currency_app/controller/CurrencyRateController.java
+++ src/main/java/ua/skorobahatyi/currency_app/controller/CurrencyRateController.java
@@ -1,13 +1,17 @@
 package ua.skorobahatyi.currency_app.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ua.skorobahatyi.currency_app.entity.dto.CurrencyRateDto;
+import ua.skorobahatyi.currency_app.service.CurrencyService;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/v1/currency_rates")
 public class CurrencyRateController {
+    private final CurrencyService currencyService;
 
     @GetMapping("/actual")
     public CurrencyRateDto getActualCurrencyRates(){
