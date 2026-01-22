3ae9dbf395463dc0035ba7e72116cef882aa5691
SkorobagatiyMykola <nick1980457@gmail.com>
2026-01-22 15:55:25 +0200

    Changed CurrencyService

--- src/main/java/ua/skorobahatyi/currency_app/service/CurrencyService.java
+++ src/main/java/ua/skorobahatyi/currency_app/service/CurrencyService.java
@@ -1,14 +1,18 @@
 package ua.skorobahatyi.currency_app.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
+import ua.skorobahatyi.currency_app.entity.CurrencyRate;
 import ua.skorobahatyi.currency_app.entity.dto.CurrencyDto;
 import ua.skorobahatyi.currency_app.repository.CurrencyRepository;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 
 @Service
@@ -23,10 +27,17 @@ public class CurrencyService {
     @Transactional(readOnly = true)
     public List<CurrencyDto> getAllCurrency() {
         return currencyRepository.findTotalCurrenciesDto();
-        /*return currencyRepository.findAll()
-                .stream()
-                .map(c->new CurrencyDto(c.getCode(),c.getName()))
-                .toList();*/
+    }
+
+    public List<CurrencyRate> getAllCurrencyRate() {
+        var jsonResponse = restTemplate.getForObject(nbuCurrencyUrl, JsonNode.class);
+
+//        List<CurrencyRate> rates = StreamSupport.stream(jsonResponse.spliterator(), true)
+//                .map(el -> new CurrencyRate(el.get(""),,))
+//                //.mapToDouble(JsonNode::asDouble)
+//                .collect(Collectors.toList());
+
+        return null;
     }
 
 }
